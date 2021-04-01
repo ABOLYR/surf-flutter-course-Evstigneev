546cf4d5a3bbe16885c7ed965ea56470f6301f10
ABOLYR <valentin.evstigneev@gmail.com>
2021-04-01 21:19:17 +0300

    Added SightCard as a widget

---
+++ lib/ui/res/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const Color white = Colors.white,
+    whiteSmoke = Color(0xfff5f5f5),
+    blue = Colors.blue,
+    green = Colors.green,
+    yellow = Colors.yellow,
+    purple = Color(0xff3b3e5b),
+    darkPurple = Color(0xff3B3E5B),
+    grey = Color(0xff7C7E92);
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:places/mocks.dart';
-import 'package:places/style/Colors.dart';
+import 'package:places/ui/res/colors.dart';
+import 'package:places/ui/screen/sight_card.dart';
 
 class SightListScreen extends StatefulWidget {
   @override
   _SightListScreenState createState() => _SightListScreenState();
 }
 
-class _SightListScreenState extends State<SightListScreen> {
+class _SightListScreenState extends State<SightListScreen> { //Screen with list of sights
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: PlacesColors.backgroundColor,
+      backgroundColor: white,
       appBar: AppBar(
-        backgroundColor: PlacesColors.backgroundColor,
+        backgroundColor: white,
         elevation: 0,
         toolbarHeight: 150,
         title: Padding(
             padding: EdgeInsets.only(left: 16, right: 16, top: 64),
             child: Text('Список\nинтересных мест',
               style: TextStyle(
-                color: Color(0xff3B3E5B),
+                color: purple,
                 height: 36 / 32,
                 fontSize: 32,
                 fontFamily: 'Roboto',
@@ -36,288 +37,9 @@ class _SightListScreenState extends State<SightListScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: Container(
-                width: 328,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            topRight: Radius.circular(12)),
-                        color: Colors.blue,
-                      ),
-                      height: 96,
-                      child: Stack(
-                        children: [
-                          Align(
-                              alignment: Alignment.topRight,
-                              child: Container(
-                                margin: EdgeInsets.only(right: 18, top: 19),
-                                color: Colors.white,
-                                width: 20,
-                                height: 18,
-                              )
-                          ),
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Container(
-                              margin: EdgeInsets.only(left: 16, top: 16),
-                              child: Text('Архитектурная доминанта',
-                                style: TextStyle(
-                                color: Colors.white,
-                                height: 18 / 14,
-                                fontSize: 14,
-                                fontFamily: 'Roboto',
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w700,
-                              ),
-                          ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: 328,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(12),
-                            bottomRight: Radius.circular(12)),
-                        color: Color(0xffF5F5F5),
-                      ),
-                      height: 92,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                              padding: EdgeInsets.only(top: 16, left: 16),
-                              child: Text(mocks[0].name,
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 2,
-                                style: TextStyle(
-                                  color: Color(0xff3B3E5B),
-                                  height: 20 / 16,
-                                  fontSize: 16,
-                                  fontFamily: 'Roboto',
-                                  fontStyle: FontStyle.normal,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                          ),
-                          Padding(
-                              padding: EdgeInsets.only(top: 2, left: 16),
-                              child: Text(mocks[0].details,
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 2,
-                                style: TextStyle(
-                                  color: Color(0xff7C7E92),
-                                  height: 18 / 14,
-                                  fontSize: 14,
-                                  fontFamily: 'Roboto',
-                                  fontStyle: FontStyle.normal,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: Container(
-                width: 328,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            topRight: Radius.circular(12)),
-                        color: Colors.green,
-                      ),
-                      height: 96,
-                      child: Stack(
-                        children: [
-                          Align(
-                              alignment: Alignment.topRight,
-                              child: Container(
-                                margin: EdgeInsets.only(right: 18, top: 19),
-                                color: Colors.white,
-                                width: 20,
-                                height: 18,
-                              )
-                          ),
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Container(
-                              margin: EdgeInsets.only(left: 16, top: 16),
-                              child: Text('Архитектурная доминанта',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  height: 18 / 14,
-                                  fontSize: 14,
-                                  fontFamily: 'Roboto',
-                                  fontStyle: FontStyle.normal,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: 328,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(12),
-                            bottomRight: Radius.circular(12)),
-                        color: Color(0xffF5F5F5),
-                      ),
-                      height: 92,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(top: 16, left: 16),
-                            child: Text(mocks[1].name,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                              style: TextStyle(
-                                color: Color(0xff3B3E5B),
-                                height: 20 / 16,
-                                fontSize: 16,
-                                fontFamily: 'Roboto',
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 2, left: 16),
-                            child: Text(mocks[1].details,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                              style: TextStyle(
-                                color: Color(0xff7C7E92),
-                                height: 18 / 14,
-                                fontSize: 14,
-                                fontFamily: 'Roboto',
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: Container(
-                width: 328,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            topRight: Radius.circular(12)),
-                        color: Colors.yellow,
-                      ),
-                      height: 96,
-                      child: Stack(
-                        children: [
-                          Align(
-                              alignment: Alignment.topRight,
-                              child: Container(
-                                margin: EdgeInsets.only(right: 18, top: 19),
-                                color: Colors.white,
-                                width: 20,
-                                height: 18,
-                              )
-                          ),
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Container(
-                              margin: EdgeInsets.only(left: 16, top: 16),
-                              child: Text('Архитектурная доминанта',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  height: 18 / 14,
-                                  fontSize: 14,
-                                  fontFamily: 'Roboto',
-                                  fontStyle: FontStyle.normal,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: 328,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(12),
-                            bottomRight: Radius.circular(12)),
-                        color: Color(0xffF5F5F5),
-                      ),
-                      height: 92,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(top: 16, left: 16),
-                            child: Text(mocks[2].name,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                              style: TextStyle(
-                                color: Color(0xff3B3E5B),
-                                height: 20 / 16,
-                                fontSize: 16,
-                                fontFamily: 'Roboto',
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 2, left: 16),
-                            child: Text(mocks[2].details,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                              style: TextStyle(
-                                color: Color(0xff7C7E92),
-                                height: 18 / 14,
-                                fontSize: 14,
-                                fontFamily: 'Roboto',
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
+            SightCard(mocks[0]),
+            SightCard(mocks[1]),
+            SightCard(mocks[2]),
           ],
         ),
       ),
